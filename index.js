6e95599683495ce61111f0d4ad96c30ff97b72f5
Bernadette Davis <bsd231@nyu.edu>
2020-02-02 21:37:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -90,7 +90,20 @@ const fi = (function() {
         return callback(a) - callback(b);
       });
     },
-    flatten: function(){},
+    
+    flatten: function(collection, shallow, newArr=[]) {
+      if (!Array.isArray(collection)) return newArr.push(collection);
+      if (shallow) {
+        for (let val of collection)
+          Array.isArray(val) ? this.unpack(newArr, val) : newArr.push(val);
+      } else {
+        for (let val of collection) {
+          this.flatten(val, false, newArr);
+        }
+      }
+      return newArr;
+    },
+    
     uniq: function(){},
     keys: function(){},
     values: function(){},
